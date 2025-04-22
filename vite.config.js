6c57baf362363ec14dc68bcd819ba3bde342e4b7
Jolo Balbin <jolo.balbin@codapayments.com>
2025-04-22 16:40:45 +0800

    github action

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -17,4 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/solarcalc/' // Replace with your repository name
 })
